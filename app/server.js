0593cc369147b07ebe5b7433b6c3cb365f70b39e
sauravyash <yAgasti@gmail.com>
2019-03-28 06:40:19 +1100

    more

--- app/server.js
+++ app/server.js
@@ -16,13 +16,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const server = new _koa.default();
 const router = new _koaRouter.default();
 const PORT = process.env.DEVENV ? 4000 : process.env.PORT;
-let latestNews = {
+let blogData = {
   updates: [{
-    title: "First Post: le début",
-    date: "1/2/2019",
-    time: "3:30pm",
+    title: "Day 1",
+    date: "5/03/2019",
+    time: "2:30pm",
     tag: "General",
-    content: "This is a multipurpose blog; a rationale to parts of my life."
+    content: "Today, I searched for inspiration when searching for a model to create. I decided to choose a superhero as it would be an interesting challenge. I didn’t think about which kind of superhero as there are countless types, and honestly deciding on a choice left me overwhelmed. I decided to find something that could get me started quickly. In Google Images, I found a replica blueprint to the original design of the Iron Man Mark 46, but the resolution was extremely low and the original image appeared to be unavailable. However, I finally had a vague direction for the idea of the model."
   }, {
     title: "Creating Iron Man in Maya: Part 1",
     date: "1/3/2019",
@@ -45,10 +45,18 @@ let latestNews = {
 
 };
 router.get('/', ctx => {
-  ctx.body = _pug.default.renderFile('pub/pages/content/index.pug', latestNews);
+  ctx.body = _pug.default.renderFile('pub/pages/content/index.pug', blogData);
+});
+router.get('/:journal', ctx => {
+  ctx.body = _pug.default.renderFile('pub/pages/content/journal.pug', blogData);
 });
 router.get('/:page_name', ctx => {
-  ctx.body = _pug.default.renderFile(`pub/pages/content/${ctx.params.page_name}.pug`, latestNews);
+  try {
+    ctx.body = _pug.default.renderFile(`pub/pages/content/${ctx.params.page_name}.pug`, blogData);
+  } catch (e) {
+    ctx.status = 404;
+    ctx.body = "Sorry, page not found";
+  }
 });
 router.get('/:page_name/:sub_page', ctx => {
   try {
--- src/server/server.js
+++ src/server/server.js
@@ -9,14 +9,14 @@ const server = new Koa()
 const router = new Router()
 const PORT = process.env.DEVENV ? 4000 : process.env.PORT
 
-let latestNews = {
+let blogData = {
   updates: [
     {
-      title: "First Post: le début",
-      date: "1/2/2019",
-      time: "3:30pm",
+      title: "Day 1",
+      date: "5/03/2019",
+      time: "2:30pm",
       tag: "General",
-      content: "This is a multipurpose blog; a rationale to parts of my life."
+      content: "Today, I searched for inspiration when searching for a model to create. I decided to choose a superhero as it would be an interesting challenge. I didn’t think about which kind of superhero as there are countless types, and honestly deciding on a choice left me overwhelmed. I decided to find something that could get me started quickly. In Google Images, I found a replica blueprint to the original design of the Iron Man Mark 46, but the resolution was extremely low and the original image appeared to be unavailable. However, I finally had a vague direction for the idea of the model."
     },
     {
       title: "Creating Iron Man in Maya: Part 1",
@@ -45,11 +45,22 @@ let latestNews = {
 
 // website root
 router.get('/', ctx => {
-  ctx.body = pug.renderFile('pub/pages/content/index.pug', latestNews)
+  ctx.body = pug.renderFile('pub/pages/content/index.pug', blogData)
+})
+
+router.get('/:journal', ctx => {
+  ctx.body = pug.renderFile('pub/pages/content/journal.pug', blogData)
 })
 
 router.get('/:page_name', ctx => {
-  ctx.body = pug.renderFile(`pub/pages/content/${ctx.params.page_name}.pug`, latestNews)
+  try{
+    ctx.body = pug.renderFile(`pub/pages/content/${ctx.params.page_name}.pug`, blogData)
+  }
+  catch(e){
+    ctx.status = 404
+    ctx.body = "Sorry, page not found"
+  }
+
 })
 
 router.get('/:page_name/:sub_page', ctx => {
